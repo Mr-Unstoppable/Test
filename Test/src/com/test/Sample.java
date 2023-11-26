d0326758f41cb8050cf7fb4278151813176657fc
Mr-Unstoppable <152058905+Mr-Unstoppable@users.noreply.github.com>
2023-11-27 05:03:06 +0530

    added line by remote

--- Test/src/com/test/Sample.java
+++ Test/src/com/test/Sample.java
@@ -2,4 +2,10 @@ package com.test;
 
 public class Sample {
 
+  public static void main(String[] args) {
+		
+		System.out.println("1st line added by local");
+    System.out.println("1st line added by remote");
+	}
+
 }
